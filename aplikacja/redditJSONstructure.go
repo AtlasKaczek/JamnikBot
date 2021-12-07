3c74ed6ca71f312d70d76c8c98073734e35bcff6
Stanisław Kryj <cookieblackxd@gmail.com>
2021-12-07 15:16:26 +0100

    made some work

---
+++ aplikacja/redditJSONstructure.go
@@ -0,0 +1,44 @@
+package aplikacja
+
+//stracture
+type Images struct {
+	Data Data `json:"data"`
+}
+
+type Data struct {
+	Children []Children `json:"children"`
+}
+
+type Children struct {
+	DataV2 DataV2 `json:"data"`
+}
+
+type DataV2 struct {
+	Subreddit string `json:"subreddit_name_prefixed"`
+	Title     string `json:"title"`
+	// Media_metadata Media_metadata `json:"media_metadata"`
+	// Gallery_data   Gallery_data   `json:"gallery_data"`
+	Url_overriden_by_dest string `json:"url_overridden_by_dest"`
+}
+
+// type Media_metadata struct {
+// 	ID ID `json:""`
+// }
+
+// type Gallery_data struct {
+// 	Items []Item `json:"items"`
+// }
+
+// type Item struct {
+// 	Media_id string `json:"media_id"`
+// }
+
+//Getters
+
+func (t Images) GetImageURL(i int) string {
+	return t.Data.Children[i].DataV2.Url_overriden_by_dest
+}
+
+// func (t Images) GetMediaId(i,j int) string {
+// 	return t.Data.Children[i].DataV2.Gallery_data.Items[j].Media_id
+// }
---
+++ rest/send.go
@@ -0,0 +1,27 @@
+package rest
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func SendGET(url string) (*[]byte, error) {
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		eror := fmt.Sprintf("GET: Bad Request StatusCode = %d", response.StatusCode)
+		return nil, errors.New(eror)
+	}
+
+	byteValue, _ := ioutil.ReadAll(response.Body)
+
+	return &byteValue, nil
+}
