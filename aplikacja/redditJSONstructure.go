6f30067271bfd562c7dffcb1066cc9dc06c66b8f
Stanisław Kryj <cookieblackxd@gmail.com>
2021-12-22 20:29:38 +0100

    add image checking function

--- aplikacja/redditJSONstructure.go
+++ aplikacja/redditJSONstructure.go
@@ -14,24 +14,11 @@ type Children struct {
 }
 
 type DataV2 struct {
-	Subreddit string `json:"subreddit_name_prefixed"`
-	Title     string `json:"title"`
-	// Media_metadata Media_metadata `json:"media_metadata"`
-	// Gallery_data   Gallery_data   `json:"gallery_data"`
+	Subreddit             string `json:"subreddit_name_prefixed"`
+	Title                 string `json:"title"`
+	PostHint              string `json:"post_hint"`
 	Url_overriden_by_dest string `json:"url_overridden_by_dest"`
 }
-
-// type Media_metadata struct {
-// 	ID ID `json:""`
-// }
-
-// type Gallery_data struct {
-// 	Items []Item `json:"items"`
-// }
-
-// type Item struct {
-// 	Media_id string `json:"media_id"`
-// }
 
 //Getters
 
@@ -41,6 +28,17 @@ func (t Images) GetImageURL(i int) string {
 
 func (t Images) GetChildrenLen() int {
 	return len(t.Data.Children)
+}
+
+func (t Images) GetImagesIndexList() []int {
+	var i_list []int
+
+	for i := 0; i < t.GetChildrenLen(); i++ {
+		if t.Data.Children[i].DataV2.PostHint == "image" {
+			i_list = append(i_list, i)
+		}
+	}
+	return i_list
 }
 
 // func (t Images) GetMediaId(i,j int) string {
